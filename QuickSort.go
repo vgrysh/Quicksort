c4a9f8ea6660a29401992b5b985bfb51f0e6a7ec
vgrysh <vvov4ikk@rambler.ru>
2016-11-13 13:26:57 +0200

    refactor my code

--- QuickSort.go
+++ QuickSort.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	list := []int{1, 5, 6, 3, 2, 6, 9}
+	list := []int{7, 0, 6, 4, 2, 45, 21, 5, 6, 3, 2, 6, 9}
 	fmt.Println(list)
 	list = sort(list)
 	fmt.Println(list)
@@ -15,34 +15,40 @@ func main() {
 
 func sort(arr []int) []int {
 	rand.Seed(int64(time.Now().Second()))
-	var lesser []int
 	if (len(arr) > 0) {
-		pivot := rand.Intn(len(arr))
-		var greater []int
-		var pivotList []int
-
-		for i := 0; i < len(arr); i++ {
-			if (arr[i] == arr[pivot] ) {
-				pivotList = append(pivotList, arr[i])
-			} else {
-				if (arr[i] > arr[pivot] ) {
-					greater = append(greater, arr[i])
-				} else {
-					lesser = append(lesser, arr[i])
-				}
-			}
-		}
-
+		lesser, pivotList, greater := distributeElements(arr)
 		greater = sort(greater)
 		lesser = sort(lesser)
-		for j := 0; j < len(pivotList); j++ {
-			lesser = append(lesser, pivotList[j])
-		}
-		for j := 0; j < len(greater); j++ {
-			lesser = append(lesser, greater[j])
-		}
-		return lesser
+		return mergeSlices(lesser, pivotList, greater)
 
 	}
-	return lesser
+	return arr
+}
+
+func mergeSlices(destination []int, source1 []int, source2 []int) []int {
+	destination = append(destination, source1...)
+	destination = append(destination, source2...)
+	return destination
+}
+
+func distributeElements(arr []int) ([]int, []int, []int,) {
+	pivot := rand.Intn(len(arr))
+	var (
+		lesser []int
+		greater []int
+		pivotList []int
+	)
+
+	for i := 0; i < len(arr); i++ {
+		if (arr[i] == arr[pivot] ) {
+			pivotList = append(pivotList, arr[i])
+		} else {
+			if (arr[i] > arr[pivot] ) {
+				greater = append(greater, arr[i])
+			} else {
+				lesser = append(lesser, arr[i])
+			}
+		}
+	}
+	return lesser, pivotList, greater
 }
